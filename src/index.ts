a7d2bda46ad0d8f41ea8055e34179bef408f2123
Tap-Naka <tap@kos.co.th>
2022-10-17 02:15:04 +0700

    fix: cors v4

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,14 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
-//     res.header(
-//       'Access-Control-Allow-Headers',
-//       'Origin, X-Requested-With, Content-Type, Accept',
-//     )
-//     next()
-//   })
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    )
+    next()
+  })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
