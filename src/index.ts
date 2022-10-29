68ffcd9194e0141741a1eb8258fd904b1cf1cd34
Nachanok-Work <nachanok.work@gmail.com>
2022-10-30 01:36:33 +0700

    feat: all flow complete

--- src/index.ts
+++ src/index.ts
@@ -24,11 +24,11 @@ app.use(cors(corsOptions));
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
     )
     next()
-  })
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -61,32 +61,67 @@ app.post('/warranty/register', (req: Request, res: Response) => {
             res.sendStatus(500);
             return;
         }
-        console.log(`connected as id ${connection.threadId}`);
         const { serialNumber, orderId, firstName, lastName, email, phoneNumber } = req.body
-        connection.query('SELECT `id` FROM `user` WHERE `email`=?', [email], (err, rows) => {
-            // console.log(rows[0].id);
-            if (rows.length == 0) {
-                connection.query('INSERT INTO `user` (`firstName`, `lastName`, `email`, `phoneNumber`) VALUES(?,?,?,?)', [firstName, lastName, email, phoneNumber], (err, rows) => {
-                    if (err) {
-                        console.log(err);
-                        res.sendStatus(500);
-                        return;
-                    }
-                })
-            }
-            else if (rows.length > 1) {
-                console.log("Duplicate email: ", [email]);
-            }
-            connection.query('INSERT INTO `warranty` (`userId`, `serialNumber`, `orderId`) SELECT `id`, ?, ? FROM `user` WHERE `email`=? ', [serialNumber, orderId, email], (err, rows) => {
-                if (err) {
-                    console.log(err);
-                    res.sendStatus(500);
-                    return;
+        console.log(`connected as id ${connection.threadId}`);
+        // check isRegistered
+        connection.query('SELECT isRegistered FROM `serialNumber` WHERE `serialNumber` = ?', [serialNumber], (err, rows) => {
+            if (rows.length != 0) {
+                if (rows[0].isRegistered) {
+                    return res
+                        .status(400)
+                        .send({ error: true, message: 'Serial number already registered' });
                 }
-            })
+                else {
+                    // register serial number
+                    connection.query('SELECT `id` FROM `user` WHERE `email`=?', [email], (err, rows) => {
+                        if (rows.length == 0) {
+                            connection.query('INSERT INTO `user` (`firstName`, `lastName`, `email`, `phoneNumber`) VALUES(?,?,?,?)', [firstName, lastName, email, phoneNumber], (err, rows) => {
+                                if (err) {
+                                    console.log(err);
+                                    res.sendStatus(500);
+                                    return;
+                                }
+                            })
+                        }
+                        else if (rows.length > 1) {
+                            console.log("Duplicate email: ", [email]);
+                        }
+                        else if (err) {
+                            console.log(err);
+                            res.sendStatus(500);
+                            return;
+                        }
+                        connection.query('INSERT INTO `warranty` (`userId`, `serialNumber`, `orderId`) SELECT `id`, ?, ? FROM `user` WHERE `email`=? ', [serialNumber, orderId, email], (err, rows) => {
+                            if (err) {
+                                console.log(err);
+                                res.sendStatus(500);
+                                return;
+                            }
+                            // set isRegistered to TRUE
+                            connection.query('UPDATE `serialNumber` SET `isRegistered` = 1 WHERE `serialNumber` = ?', [serialNumber], (err, rows) => {
+                                if (err) {
+                                    console.log(err);
+                                    res.sendStatus(500);
+                                    return;
+                                }
+                            })
+                        })
+                    })
+                    connection.release()
+                    res.sendStatus(201)
+                }
+            }
+            else if (err) {
+                console.log(err);
+                res.sendStatus(500);
+                return;
+            }
+            else {
+                return res
+                    .status(400)
+                    .send({ error: true, message: 'Invalid serial number' });
+            }
         })
-        connection.release()
-        res.sendStatus(201)
     })
 })
 
@@ -96,47 +131,59 @@ app.get('/warranty/:serialNumber', (req: Request, res: Response, next: NextFunct
         return res
             .status(400)
             .send({ error: true, message: 'Please provide serial number' })
-    } 
+    }
     else {
         dbConnection.getConnection((err, connection) => {
             if (err) throw err
             console.log(`connected as id ${connection.threadId}`);
             // check exist serial number
-            connection.query('SELECT IFNULL(id, 0) FROM `serialNumber` WHERE `serialNumber` = ?', [serialNumber], (err, rows) => {
-                if (!err) {
-                    if (rows[0] != null) {
-                        // check is registered
-                        connection.query('SELECT IFNULL(id, 0) FROM `serialNumber` WHERE `serialNumber` = ?', [serialNumber], (err, rows) => {
-
+            connection.query('SELECT isRegistered FROM `serialNumber` WHERE `serialNumber` = ?', [serialNumber], (err, rows) => {
+                if (rows.length != 0) {
+                    if (rows[0].isRegistered) {
+                        // slice SN
+                        const _serie = serialNumber.slice(0, 3);
+                        const _stoneCode = serialNumber.slice(3, 4);
+                        const _materialCode = serialNumber.slice(4, 5);
+                        console.log(_serie, _stoneCode, _materialCode);
+                        
+                        // return warranty detail
+                        connection.query('SELECT warranty.serialNumber, warranty.orderId, user.firstName, user.lastName, user.email, user.phoneNumber FROM warranty INNER JOIN user ON warranty.userId = user.Id WHERE warranty.serialNumber = ?', [serialNumber], (err, rows) => {
+                            if (!err) {
+                                connection.release()
+                                res.status(201)
+                                res.send(rows)
+                            } else {
+                                console.log(err)
+                                res.sendStatus(500);
+                                return;
+                            }
                         })
                     }
                     else {
                         return res
-                        .status(400)
-                        .send({ error: true, message: 'Serial number does not exist' })
+                            .status(400)
+                            .send({ error: true, message: 'Serial number unregistered' });
                     }
-                } else {
+                }
+                else if (err) {
                     console.log(err);
+                    res.sendStatus(500);
+                    return;
+                }
+                else {
+                    return res
+                        .status(400)
+                        .send({ error: true, message: 'Invalid serial number' });
                 }
             })
-
-            // connection.query('SELECT warranty.serialNumber, warranty.orderId, user.firstName, user.lastName, user.email, user.phoneNumber FROM warranty INNER JOIN user ON warranty.userId = user.Id WHERE warranty.serialNumber = ?', [serialNumber], (err, rows) => {
-            //     connection.release()
-            //     if (!err) {
-            //         res.send(rows)
-            //     } else {
-            //         console.log(err)
-            //     }
-            // })
         })
     }
 })
 
-// test
-// console.log("testing");
-// createSerialNumber("AMY","A","A");
 app.post('/warranty/admin/createSerialNumber', (req: Request, res: Response) => {
-    createSerialNumber("AMY","A","A");
+    const { serie, stoneCode, materialCode } = req.body
+    createSerialNumber(serie, stoneCode, materialCode);
+    res.sendStatus(200);
 })
 
 
